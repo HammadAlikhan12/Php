c439d911fe7f695fdf9d44036deed055e07a94b1
HammadAlikhan12 <103217942+HammadAlikhan12@users.noreply.github.com>
2022-04-18 00:07:22 +0500

    Add files via upload

---
+++ Navbar.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar main-nav border-less fixed-top navbar-expand-lg p-0">
+  <div class="container-fluid p-0">
+      <!-- logo -->
+      <a class="navbar-brand" href="index.html">
+        <img src="images/logo.png" alt="logo">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="fa fa-bars"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item ">
+          <a class="nav-link" href="Index.php"  >Home
+            <span>/</span>
+          </a>
+          
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Schedule.php">Schedule
+            <span>/</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Profile.php">Profile<span>/</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Add_data.php">Add-data<span>/</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="About.php">About<span>/</span></a>
+        </li>
+       
+        <li class="nav-item">
+          <a class="nav-link" href="Contact.php">Contact</a>
+        </li>
+      </ul>
+      <a href="Login.php" class="Ticket">Login</a>
+      <div Style="width:20px"></div>
+    <a href="Logout.php" class="Ticket">Logout</a>
+      <div Style="width:20px"></div>
+      <a href="Register.php" class="Ticket">Register</a>
+        
+      
+      </div>
+  </div>
+</nav>
+
+</body>
+</html>
